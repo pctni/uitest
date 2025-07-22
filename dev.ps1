be0f502b391e59c373213fa8c44ed40453f4bb90
roblinlovelace <rob00x@gmail.com>
2025-07-22 15:49:23 +0100

    Rm all

--- dev.ps1
+++
@@ -1,6 +0,0 @@
-# Development script for Windows
-Write-Host "Installing dependencies..." -ForegroundColor Green
-deno install --allow-scripts=npm:esbuild,npm:@esbuild/win32-x64
-
-Write-Host "Starting development server..." -ForegroundColor Green
-deno task dev
